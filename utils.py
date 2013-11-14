93149e56d863a48c3abe7b12602bf960c2977644
uril <uri@london.org.il>
2013-11-14 08:28:37 +0200

    all entities in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 from google.appengine.ext import ndb
+from google.appengine.ext.blobstore import BlobInfo
 from model import Firm, Project, Image
 
 def hierarchy():
@@ -17,4 +18,24 @@ def hierarchy():
 
     print json.dumps(projects)
 
+
+
+def all_entities():
+    k = ndb.Key('Firm', 'frl')
+    #return the iterator
+    return ndb.Query(ancestor=k).iter(keys_only=True)
+
+def print_all_entities():
+    for x in all_entities():
+        print x
+
+
+def delete_all_images():
+    k = ndb.Key('Firm', 'frl')
+    for img in Image.query(ancestor=k).iter():
+        print img.key.id()
+	for bk in [img.small_blob_key, img.large_blob_key]:
+            if BlobInfo.get(bk):
+                BlobInfo.get(bk).delete()
+        img.key.delete()
 
